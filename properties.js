697ba23c0b246935d1288e41b73f8c7ffa782e21
chenxingyu <chenxingyu_92@qq.com>
2010-05-27 12:32:56 +0800

    First Commit

---
+++ properties.js
@@ -0,0 +1,22 @@
+var Properties = new Class({
+	set: function(key, value)
+	{
+		if (!this.properties) return null;
+		if (!this.properties[key]) return null;
+		if (!this.properties[key].set) return null;
+		if ($type(this.properties[key].set) != 'function') return null;
+		
+		this.properties[key].set.bind(this).run(value);
+		return value;
+	},
+	
+	get: function(key)
+	{
+		if (!this.properties) return null;
+		if (!this.properties[key]) return null;
+		if (!this.properties[key].get) return null;
+		if ($type(this.properties[key].get) != 'function') return null;
+		
+		return this.properties[key].get.bind(this).run();
+	}
+});
